875d33cd4b9c22b4548ad7ca8c0b75f7e93cd860
Vishhh <91872471+vishaalinimanikandan@users.noreply.github.com>
2026-02-10 11:01:04 +0000

    Time: 23 ms (31.77%), Space: 26 MB (51.02%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hset=set()
+        for i in nums:
+            if i in hset:
+                return True
+            else:
+                hset.add(i)
+        return False
